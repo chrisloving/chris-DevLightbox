1b4ae93a3b0e6bd78827192a9edbd85782b330da
chrisloving <chrisloving@hotmail.com>
2015-02-04 07:55:44 -0700

    update

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
 //Problem: User when clicking on image goes to a dead end
 //Solution: Create an overlay with the large image - Lightbox
 
+//example: var $body = $('body');
+$(document).ready(function() {
 
+//Capture Click event on image
+//Orevent new page from opening on click
 
-
+	$('#imageGallery a').on('click', function(event) {
+		event.preventDefault();
+		console.log('dont do it');
+	})
+});
 
 
 
